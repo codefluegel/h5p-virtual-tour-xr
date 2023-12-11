0dc757699e6886460ad1dfeb8c2c8caa4cc29cc8
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2023-12-11 15:01:48 +0100

    add tooltip to zoom buttons

--- scripts/components/ZoomButtons/ZoomButtons.js
+++ scripts/components/ZoomButtons/ZoomButtons.js
@@ -3,8 +3,8 @@ import './ZoomButtons.scss';
 
 export default class ZoomButtons extends React.Component {
   /**
-   * @class
-   * @param {object} props React props.
+   * @class ZoomButtons
+   * @param {object} props ZoomButtons props.
    */
   constructor(props) {
     super(props);
@@ -12,26 +12,36 @@ export default class ZoomButtons extends React.Component {
   }
 
   /**
-   * React render function.
+   * Render ZoomButtons.
    * @returns {object} JSX element.
    */
   render() {
     return (
       <div className='h5p-ndla-virtual-tour-zoom-buttons'>
-        <button
-          aria-label={ this.props.labelZoomIn }
-          className='h5p-ndla-virtual-tour-zoom-button zoom-in'
-          onClick={ this.props.onZoomIn }
-          tabIndex={ this.props.tabIndex }
-          disabled={ this.props.isZoomInDisabled }
-        />
-        <button
-          aria-label={ this.props.labelZoomOut }
-          className='h5p-ndla-virtual-tour-zoom-button zoom-out'
-          onClick={ this.props.onZoomOut }
-          tabIndex={ this.props.tabIndex }
-          disabled={ this.props.isZoomOutDisabled }
-        />
+        <div className='h5p-ndla-virtual-tour-zoom-button-wrapper'>
+          <button
+            aria-label={ this.props.labelZoomIn }
+            className='h5p-ndla-virtual-tour-zoom-button zoom-in'
+            onClick={ this.props.onZoomIn }
+            tabIndex={ this.props.tabIndex }
+            disabled={ this.props.isZoomInDisabled }
+          />
+          <div className='tooltip' aria-hidden='true'>
+            <div className='text-wrap'>{ this.props.labelZoomIn }</div>
+          </div>
+        </div>
+        <div className='h5p-ndla-virtual-tour-zoom-button-wrapper'>
+          <button
+            aria-label={ this.props.labelZoomOut }
+            className='h5p-ndla-virtual-tour-zoom-button zoom-out'
+            onClick={ this.props.onZoomOut }
+            tabIndex={ this.props.tabIndex }
+            disabled={ this.props.isZoomOutDisabled }
+          />
+          <div className='tooltip' aria-hidden='true'>
+            <div className='text-wrap'>{ this.props.labelZoomOut }</div>
+          </div>
+        </div>
       </div>
     );
   }
